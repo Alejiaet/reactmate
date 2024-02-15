d9e988a3dd2345be7d4db45f9fb7d5724f4fb85f
alejiaet <alejiaet@gmail.com>
2024-02-15 08:12:16 -0500

    prueba registro modified:   src/pages/Cart.tsx modified:   src/pages/Signup.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,14 @@
+function Productcart(){
+    return(
+ <div className="product">
+    <p>me pica el perineo</p></div>)
+}
 const Cart = () => {
     return(
-        <div className="">
-            Cart
-        </div>
+        <div className="border-2 shadow-cyan-500/50">
+            cart
+            <Productcart/>
+                </div>
     )
 }
 
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,6 +2,16 @@ export default function SignUp(){
     return(
         <>
             Singup
+            Selecciones una imagen:
+            <input type="image" className="border-2 border-furnipurple bg-violet-600 rounded p-1"/>
+            Local: <input type="checkbox" className="accent-furnipurple"/>
+            Empresa: <input type="checkbox" className="accent-furnipurple"/>
+            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Nombre de Usuario/Empresa"/>
+            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Contraseña"/>
+            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Confirmar Contraseña"/>
+            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Telefono"/>
+            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Correo Electronico"/>
+
         </>
     )
 }
