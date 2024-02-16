ab4c623469f9498dfca4aac0b15d24496ccc53f6
alejiaet <alejiaet@gmail.com>
2024-02-16 13:39:53 -0500

    signup modified:   src/pages/Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,8 +3,10 @@ export default function SignUp(){
     return(
         <>
             Singup
-            Seleccione una imagen:
-            <input type="file"  className="border-2 border-furnipurple p-1"/>
+
+<div className="bg-slate-400 ">
+Seleccione una imagen:
+            <input type="file"  className="border-2 border-furnipurple p-1" accept="image/*"/>
             Local: <input type="checkbox" className="accent-furnipurple"/>
             Empresa: <input type="checkbox" className="accent-furnipurple"/>
             <input type="text" className="border-2 border-furnipurple rounded p-1" placeholder="Nombre de Usuario/Empresa"/>
@@ -13,6 +15,10 @@ export default function SignUp(){
             <input type="number" className="border-2 border-furnipurple rounded p-1" placeholder="Telefono"/>
             <input type="email" className="border-2 border-furnipurple rounded p-1" placeholder="Correo Electronico"/>
 
+</div>
+            
+
+            
         </>
     )
 }
