4709f7602264b88e7c7e35048ef659243681568d
alejiaet <alejiaet@gmail.com>
2024-02-16 11:58:22 -0500

    Signup 	modified:   src/pages/Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,16 +1,17 @@
 export default function SignUp(){
+ 
     return(
         <>
             Singup
-            Selecciones una imagen:
-            <input type="image" className="border-2 border-furnipurple bg-violet-600 rounded p-1"/>
+            Seleccione una imagen:
+            <input type="file"  className="border-2 border-furnipurple p-1"/>
             Local: <input type="checkbox" className="accent-furnipurple"/>
             Empresa: <input type="checkbox" className="accent-furnipurple"/>
-            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Nombre de Usuario/Empresa"/>
-            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Contraseña"/>
-            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Confirmar Contraseña"/>
-            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Telefono"/>
-            <input type="text" className="border-2 border-furnipurple bg-violet-600 rounded p-1" placeholder="Correo Electronico"/>
+            <input type="text" className="border-2 border-furnipurple rounded p-1" placeholder="Nombre de Usuario/Empresa"/>
+            <input type="password" className="border-2 border-furnipurple rounded p-1" placeholder="Contraseña"/>
+            <input type="password" className="border-2 border-furnipurple rounded p-1" placeholder="Confirmar Contraseña"/>
+            <input type="number" className="border-2 border-furnipurple rounded p-1" placeholder="Telefono"/>
+            <input type="email" className="border-2 border-furnipurple rounded p-1" placeholder="Correo Electronico"/>
 
         </>
     )
